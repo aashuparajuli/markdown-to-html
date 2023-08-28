44fd08d4aade953bddc4876a94130c887d2d1be9
Aashu Parajuli <aashuparajuli@gmail.com>
2023-08-28 18:49:09 -0400

    check and process h2 and h3 tags

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-fn add_h1_header(str: String) -> String {
-    format!("<h1>{}</h1>", str)
-}
-
-fn process_h1(str: String) -> String{
+fn process_headers(str: String) -> String{
     //if first characters are 'h1', then add the h1 tags
     if &str[0..2] == "# "{
         let remaining_str = &str[2..];
         format!("<h1>{}</h1>", remaining_str)
-    } else{
+    } 
+    else if &str[0..3] == "## "{
+        let remaining_str = &str[3..];
+        format!("<h2>{}</h2>", remaining_str)
+    }
+    else if &str[0..4] == "### "{
+        let remaining_str = &str[4..];
+        format!("<h3>{}</h3>", remaining_str)
+    }
+    else{
          str
     }
 }
@@ -20,26 +25,41 @@ fn main() {
 mod tests {
     use super::*;
     #[test]
-    fn test_add_h1_header(){
-        let input_str= String::from("Here is a header");
-        let expected_result =  String::from("<h1>Here is a header</h1>");
-        let actual_result = add_h1_header(input_str);
-        assert_eq!(actual_result, expected_result);
-    }
-    #[test]
     fn convert_h1_header(){
         //valid string should receive tags
         let input_str= String::from("# Here is a header");
         let expected_result =  String::from("<h1>Here is a header</h1>");
-        let actual_result = process_h1(input_str);
+        let actual_result = process_headers(input_str);
         assert_eq!(actual_result, expected_result);
     }
     #[test]
     fn convert_improper_h1_header(){
         //string with space before pound sign should not be converted
         let input_str= String::from(" # Here is a header");
-        let actual_result: String = process_h1(input_str.clone());
+        let actual_result: String = process_headers(input_str.clone());
         assert_eq!(actual_result, input_str);
-    }
 
+        let input_str_2= String::from("#Here is a header");
+        let actual_result_2: String = process_headers(input_str_2.clone());
+        assert_eq!(actual_result_2, input_str_2);
+    }
+    #[test]
+    fn convert_h2_header(){
+        //valid string should receive tags
+        let input_str= String::from("## Here is a header");
+        let expected_result =  String::from("<h2>Here is a header</h2>");
+        let actual_result = process_headers(input_str);
+        assert_eq!(actual_result, expected_result);
+    }
+    #[test]
+    fn convert_improper_h2_header(){
+        //string with space before pound sign should not be converted
+        let input_str= String::from(" ## Here is a header");
+        let actual_result: String = process_headers(input_str.clone());
+        assert_eq!(actual_result, input_str);
+
+        let input_str_2= String::from("##Here is a header");
+        let actual_result_2: String = process_headers(input_str_2.clone());
+        assert_eq!(actual_result_2, input_str_2);
+    }
 }
