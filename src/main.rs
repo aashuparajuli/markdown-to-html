4b1a354f09c7dbb0ba80959b590aa77afa1c335e
Aashu Parajuli <aashuparajuli@gmail.com>
2023-08-28 21:14:37 -0400

    added function to process italics

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,45 @@ fn process_headers(str: String) -> String{
     }
 }
 
+fn process_italics(str: String) -> String{
+    let mut result = String::new();
+    let mut stack: Vec<char> = Vec::new();
+    let mut buffer = String::new();
+    for c in str.chars(){
+        /*
+         cases:
+         - not in italics, adding a char
+         - switching into italics
+         - in italics, adding a char
+         - switching out of italics
+         */
+        if c == '*'{ //switching in or out of italics
+            //if top of stack is *, then we are switching out
+            if stack.last() == Some(&'*') {
+                //pop the asterisk, update the buffer correctly
+                stack.pop();
+                //update the buffer with italicized text
+                buffer = format!("<i>{buffer}</i>");
+            }
+            else{//else, we are switching into italics
+                stack.push('*');//update the stack
+            }
+            result.push_str(buffer.as_str());//push the current contents of the buffer
+            buffer = String::new();//reset the buffer to being empty
+        }
+        else{
+            buffer.push(c);
+        }
+    }
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+
+   let italics_result: String = process_italics(String::from("new *string*"));
+   let header_result: String = process_headers(String::from("# new string"));
+   println!("{}", italics_result);
+   println!("{}", header_result);
 }
 
 #[cfg(test)]
@@ -61,5 +98,13 @@ mod tests {
         let input_str_2= String::from("##Here is a header");
         let actual_result_2: String = process_headers(input_str_2.clone());
         assert_eq!(actual_result_2, input_str_2);
+    }
+    #[test]
+    fn convert_italics(){
+        //string with space before pound sign should not be converted
+        let input_str= String::from("some *text*");
+        let expected_result =  String::from("some <i>text</i>");
+        let actual_result = process_italics(input_str);
+        assert_eq!(actual_result, expected_result);
     }
 }
