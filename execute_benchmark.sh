7df53129cdb8b46ed8fae5529f0a0ddf0a88a5c0
Aashu Parajuli <aashuparajuli@gmail.com>
2023-08-31 14:41:56 -0400

    benchmark script deletes output files between runs

--- execute_benchmark.sh
+++ execute_benchmark.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+cargo build
 total_execution_time=0
-sample_size=100
-for i in {1..100}
+sample_size=200
+for i in {1..200}
 do
     output=$(command time cargo run 2>&1 >/dev/null)
     execution_time=$(echo "$output" | ggrep -Po "\d+.\d+\s(?=real)")
     total_execution_time=$(echo "$total_execution_time + $execution_time" | bc)
+    #delete the file that was generated
+    rm benchmarks/benchmark1/output.html
 done
-average_execution_time=$(echo "scale=2;$total_execution_time / $sample_size" | bc)
+average_execution_time=$(echo "scale=3;$total_execution_time / $sample_size" | bc)
 echo "$average_execution_time"
