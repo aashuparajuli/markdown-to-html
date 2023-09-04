9802f641f80b6671326b2dc6322e9d047de7b22d
Aashu Parajuli <aashuparajuli@gmail.com>
2023-09-04 15:31:48 -0400

    reorganized location of testing files

--- check.sh
+++ check.sh
@@ -1,7 +1,13 @@
-cargo run -- $1 $2 1> /dev/null
-diff_lines=$(diff $2 $3 | wc -l)
+input_folder="test"
+input_file="test/input.txt"
+output_file="test/output.txt"
+expected_output_file="test/expected.txt"
+
+cargo run -- $input_file $output_file 1> /dev/null
+diff_lines=$(diff $output_file $expected_output_file | wc -l)
 
 if [ ! $diff_lines -eq 0 ]
 then
     echo "Test failed: invalid file was generated"
+    diff $output_file $expected_output_file
 fi
