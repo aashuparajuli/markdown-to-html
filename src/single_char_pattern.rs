ae510c12587b02d9ae60449a7059d36303877f1f
Aashu Parajuli <aashuparajuli@gmail.com>
2023-09-29 00:22:38 -0400

    parse underscore and asterisk tokens with a single function

--- src/single_char_pattern.rs
+++ src/single_char_pattern.rs
@@ -1,109 +1,36 @@
-trait FormattingToken {
-    fn get_text(&self) -> String;
-}
-
-enum ItalicsAsteriskState {
-    Italics,
-    Plaintext,
-}
-
 //basic ideas: user passes in an enum that impl FormattingToken
 //then all other operations work the same
 
 //or user passes in a character, then a TextState is built off that
 //or user passes in a function to check if a character is a token, then that is used to build a function
 mod single_char_parser {
-    use super::FormattingToken;
     trait FormattedText<'a> {
         fn get_text(&'a self) -> String;
         fn is_formatting_token(&self, c: char) -> bool;
     }
-    mod italics_underscore {
-        use super::FormattedText;
-        pub struct ItalicsUnderscoreText<'a> {
-            formatted: bool,    //is either formatted or not
-            substring: &'a str, //the text to format
-        }
-        impl FormattedText<'_> for ItalicsUnderscoreText<'_> {
-            fn get_text<'a>(&'a self) -> String {
-                match self.formatted {
-                    true => format!("<i>{}</i>", self.substring),
-                    false => self.substring.to_string(),
-                }
-            }
-            fn is_formatting_token(&self, c: char) -> bool {
-                match c {
-                    '_' => true,
-                    _ => false,
-                }
-            }
-        }
-        impl ItalicsUnderscoreText<'_> {
-            pub fn new(formatted: bool, substring: &str) -> ItalicsUnderscoreText {
-                ItalicsUnderscoreText {
-                    formatted,
-                    substring,
-                }
-            }
-        }
-    }
-    mod italics_asterisk {
-        use super::FormattedText;
-        pub struct ItalicsAsteriskText<'a> {
-            formatted: bool, //is either formatted or not. FUTURE: convert this into enum so that all patterns can be parsed simaeltaenesouly
-            substring: &'a str, //the text to format
-        }
-        impl FormattedText<'_> for ItalicsAsteriskText<'_> {
-            fn get_text<'a>(&'a self) -> String {
-                match self.formatted {
-                    true => format!("<i>{}</i>", self.substring),
-                    false => self.substring.to_string(),
-                }
-            }
-            fn is_formatting_token(c: char) -> bool {
-                match c {
-                    '*' => true,
-                    _ => false,
-                }
-            }
-        }
-        impl ItalicsAsteriskText<'_> {
-            pub fn new(formatted: bool, substring: &str) -> ItalicsAsteriskText {
-                ItalicsAsteriskText {
-                    formatted,
-                    substring,
-                }
-            }
-        }
-    }
-    fn is_formatting_token(c: char) -> bool {
-        match c {
-            '*' => true,
-            _ => false,
-        }
-    }
-    pub struct FormatText {
+
+    pub struct FormatText<'a> {
         formatted: bool,
-        substring: &str,
+        substring: &'a str,
     }
-    impl FormatText {
+    impl FormatText<'_> {
         fn new(formatted: bool, substring: &str) -> FormatText {
-            ItalicsAsteriskText {
+            FormatText {
                 formatted,
                 substring,
             }
         }
     }
-    fn get_text(&f: FormatText) -> String {
+    fn get_text(f: &FormatText) -> String {
         match f.formatted {
-            true => format!("<i>{}</i>", self.substring),
-            false => self.substring.to_string(),
+            true => format!("<i>{}</i>", f.substring),
+            false => f.substring.to_string(),
         }
     }
-    pub fn process_single_char_formats(str: &str) -> String {
+    pub fn process_single_char_formats(str: &str, is_formatting_token: fn(char) -> bool) -> String {
         //make it generic over any type that implements
         let mut result: String = String::new();
-        let mut stack: Vec<&dyn FormattedText> = Vec::new();
+        let mut stack: Vec<FormatText> = Vec::new();
         let mut parsing_formatted_text: bool = false;
         let mut start_idx: usize = 0;
         if str.is_empty() {
@@ -147,17 +74,16 @@ mod single_char_parser {
             }
             if parsing_formatted_text && is_formatting_token(c) {
                 //construct a FormattedText struct storing TextState::Italics, append it to the stack
-                let italics_text = ItalicsUnderscoreText::new(true, &str[start_idx..curr_idx]);
-                stack.push(&italics_text);
+                let italics_text = FormatText::new(true, &str[start_idx..curr_idx]);
+                stack.push(italics_text);
                 start_idx = curr_idx;
                 parsing_formatted_text = false;
             } else if !parsing_formatted_text && is_formatting_token(c) {
                 //construct a FormattedText struct storing TextState::Plaintext, append it to the stack
                 //let italics_text = FormattedText::new(TextState::Plaintext, start_idx, curr_idx);
-                let italics_text: ItalicsUnderscoreText =
-                    ItalicsUnderscoreText::new(false, &str[start_idx..curr_idx]);
+                let italics_text: FormatText = FormatText::new(false, &str[start_idx..curr_idx]);
 
-                stack.push(&italics_text);
+                stack.push(italics_text);
                 //increment start pointer
                 start_idx = curr_idx + 1;
                 //switch into parsing italics mode
@@ -168,28 +94,34 @@ mod single_char_parser {
         if parsing_formatted_text {
             //println!("found unmatched asterisk");
             let plain_text = FormatText::new(false, &str[start_idx - 1..]);
-            stack.push(&mut plain_text);
+            stack.push(plain_text);
         } else if start_idx != str.len() - 1 {
             //if a plaintext substring reaches the end of the fullstring, then push the entire substring to the stack
             //println!("found unterminated plain text");
             let plain_text = FormatText::new(false, &str[start_idx..]);
-            stack.push(&mut plain_text);
+            stack.push(plain_text);
         }
         stack
             .iter()
-            .for_each(|state| result.push_str(&(state).get_text()));
+            .for_each(|subsection| result.push_str(&get_text(subsection)));
         result
     }
 
     #[cfg(test)]
     mod italics_underscore_test {
+        fn is_underscore_token(c: char) -> bool {
+            match c {
+                '_' => true,
+                _ => false,
+            }
+        }
         use super::*;
         #[test]
         fn convert_italics() {
             //string with space before pound sign should not be converted
             let input_str = String::from("some _text_");
             let expected_result = String::from("some <i>text</i>");
-            let actual_result = process_single_char_formats(&input_str);
+            let actual_result = process_single_char_formats(&input_str, is_underscore_token);
             assert_eq!(actual_result, expected_result);
         }
         #[test]
@@ -197,7 +129,7 @@ mod single_char_parser {
             //string with space before pound sign should not be converted
             let input_str = String::from("plain text");
             let expected_result = String::from("plain text");
-            let actual_result = process_single_char_formats(&input_str);
+            let actual_result = process_single_char_formats(&input_str, is_underscore_token);
             assert_eq!(actual_result, expected_result);
         }
         #[test]
@@ -205,7 +137,7 @@ mod single_char_parser {
             //string with space before pound sign should not be converted
             let input_str = String::from("some _text _");
             let expected_result = String::from("some <i>text </i>");
-            let actual_result = process_single_char_formats(&input_str);
+            let actual_result = process_single_char_formats(&input_str, is_underscore_token);
             assert_eq!(actual_result, expected_result);
         }
         #[test]
@@ -213,7 +145,8 @@ mod single_char_parser {
             //string with space before pound sign should not be converted
             let input_str = String::from("some _ text _");
             let expected_result = String::from("some _ text _");
-            let actual_result: String = process_single_char_formats(&input_str);
+            let actual_result: String =
+                process_single_char_formats(&input_str, is_underscore_token);
             assert_eq!(actual_result, expected_result);
         }
         #[test]
@@ -221,18 +154,25 @@ mod single_char_parser {
             //string with space before pound sign should not be converted
             let input_str = String::from("some __text");
             let expected_result = String::from("some __text");
-            let actual_result: String = process_single_char_formats(&input_str);
+            let actual_result: String =
+                process_single_char_formats(&input_str, is_underscore_token);
             assert_eq!(actual_result, expected_result);
         }
     }
     mod italics_asterisk_test {
+        fn is_asterisk_token(c: char) -> bool {
+            match c {
+                '*' => true,
+                _ => false,
+            }
+        }
         use super::*;
         #[test]
         fn convert_italics() {
             //string with space before pound sign should not be converted
             let input_str = String::from("some *text*");
             let expected_result = String::from("some <i>text</i>");
-            let actual_result = process_single_char_formats(&input_str);
+            let actual_result = process_single_char_formats(&input_str, is_asterisk_token);
             assert_eq!(actual_result, expected_result);
         }
         #[test]
@@ -240,7 +180,7 @@ mod single_char_parser {
             //string with space before pound sign should not be converted
             let input_str = String::from("plain text");
             let expected_result = String::from("plain text");
-            let actual_result = process_single_char_formats(&input_str);
+            let actual_result = process_single_char_formats(&input_str, is_asterisk_token);
             assert_eq!(actual_result, expected_result);
         }
         #[test]
@@ -248,7 +188,7 @@ mod single_char_parser {
             //string with space before pound sign should not be converted
             let input_str = String::from("some *text *");
             let expected_result = String::from("some <i>text </i>");
-            let actual_result = process_single_char_formats(&input_str);
+            let actual_result = process_single_char_formats(&input_str, is_asterisk_token);
             assert_eq!(actual_result, expected_result);
         }
         #[test]
@@ -256,7 +196,7 @@ mod single_char_parser {
             //string with space before pound sign should not be converted
             let input_str = String::from("some * text *");
             let expected_result = String::from("some * text *");
-            let actual_result: String = process_single_char_formats(&input_str);
+            let actual_result: String = process_single_char_formats(&input_str, is_asterisk_token);
             assert_eq!(actual_result, expected_result);
         }
         #[test]
@@ -264,7 +204,7 @@ mod single_char_parser {
             //string with space before pound sign should not be converted
             let input_str = String::from("some **text");
             let expected_result = String::from("some **text");
-            let actual_result: String = process_single_char_formats(&input_str);
+            let actual_result: String = process_single_char_formats(&input_str, is_asterisk_token);
             assert_eq!(actual_result, expected_result);
         }
     }
