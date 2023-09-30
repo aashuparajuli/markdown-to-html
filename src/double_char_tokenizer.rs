93d907abd566a4058b595a00c8beb3fbc791f1f7
Aashu Parajuli <aashuparajuli@gmail.com>
2023-09-29 20:35:19 -0400

    more attempts at tokenizer

--- src/double_char_tokenizer.rs
+++ src/double_char_tokenizer.rs
@@ -3,7 +3,7 @@ pub enum Token {
     Plaintext,
     Asterisk,
     Space,
-    DoubleAsterisk, //each character, except double asterisk gets it own character
+    //DoubleAsterisk, //each character, except double asterisk gets it own character
 }
 #[derive(Clone, Copy)]
 enum CharType {
@@ -24,22 +24,43 @@ pub fn double_char_tokenizer(str: &str) -> Vec<Token> {
     use Token;
     //make it generic over any type that implements
     let mut token_stream: Vec<Token> = Vec::new();
-    let mut prev_char: Option<CharType> = None;
+    let mut curr_token: Option<Token> = None;
 
-    let mut _start_idx: usize = 0;
-    for (_i, c) in str.char_indices() {
-        let curr_char = CharType::new(c);
-        match (prev_char, curr_char) {
-            (_, CharType::Asterisk) if matches!(prev_char, Some(CharType::Asterisk)) => {
-                //remove pop token
-                token_stream.pop();
-                token_stream.push(Token::DoubleAsterisk)
+    let mut start_idx: usize = 0;
+    for (i, c) in str.char_indices() {
+        let next_char = CharType::new(c);
+
+        //preconditions, dealing with altering previous elements
+        //if prev char is not plaintext && curr char is plaintext => set start_idx
+        //if prev char is plaintext && curr char is not plaintext => use start_idx and i to get substring
+        //if prev char is plaintext && curr char is plaintext, do nothing, let i continue growing
+        //do nothing
+
+        //otherwise, just push to stack
+
+        match (curr_token, next_char) {
+            (None, _ ) => {
+                //just push the token
+                let next_token = match next_char {
+                    CharType::Asterisk => Token::Asterisk,
+                    CharType::Plaintext => Token::Plaintext ,
+                    CharType::Space => Token::Space,
+                };
+                curr_token = Some(next_token.clone());
+                token_stream.push(next_token)
+            }
+            (Some(Token::Plaintext), CharType::Plaintext) => {
+                //do nothing
+            }
+            (Some(Token::Plaintext), CharType::Asterisk)  | (Some(Token::Plaintext), CharType::Space)=> {
+                //end plaintext, push it
+                let _tuple = (start_idx, i);
+                //push asterisk or plaintext
             }
             (_, CharType::Asterisk) => token_stream.push(Token::Asterisk),
             (_, CharType::Plaintext) => token_stream.push(Token::Plaintext),
             (_, CharType::Space) => token_stream.push(Token::Space),
         };
-        prev_char = Some(curr_char.clone());
     }
     token_stream
 }
@@ -75,12 +96,12 @@ mod test_tokenizer {
         //string with space before pound sign should not be converted
         let input_str = "some**";
         let actual_result: Vec<Token> = double_char_tokenizer(input_str);
-        assert_eq!(actual_result.len(), 5);
+        assert_eq!(actual_result.len(), 6);
         assert!(matches!(actual_result[0], Token::Plaintext));
         assert!(matches!(actual_result[1], Token::Plaintext));
         assert!(matches!(actual_result[2], Token::Plaintext));
         assert!(matches!(actual_result[3], Token::Plaintext));
-        assert!(matches!(actual_result[4], Token::DoubleAsterisk));
+        //assert!(matches!(actual_result[4], Token::DoubleAsterisk));
     }
     #[test]
     fn mixed() {
