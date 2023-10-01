cd7f27e8d99e1a698d6fafd1b6356fdfb29c861c
Aashu Parajuli <aashuparajuli@gmail.com>
2023-10-01 19:52:21 -0400

    renaming files within single_char_parse

--- src/single_char_parse.rs
+++ src/single_char_parse.rs
@@ -1,6 +1,6 @@
-mod single_char_wrappers;
+mod wrapper;
 pub mod tag;
 pub mod single_char_parser;
-pub use single_char_wrappers::inline_code::process_inline_code;
-pub use single_char_wrappers::italics::process_asterisk as italics_asterisk;
-pub use single_char_wrappers::italics::process_underscore as italics_underscore;
+pub use wrapper::inline_code::process_inline_code;
+pub use wrapper::italics::process_asterisk as italics_asterisk;
+pub use wrapper::italics::process_underscore as italics_underscore;
