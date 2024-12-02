031bd15d1f95a75399640aa066660a8e2910c39b
Aashu Parajuli <aashuparajuli@gmail.com>
2024-12-01 22:11:22 -0500

    merge line-level-parsing into develop

--- src/single_char_parse.rs
+++ src/single_char_parse.rs
@@ -1,8 +1,7 @@
-mod wrapper;
+pub mod single_char_parser;
 mod token;
 mod tokenizer;
-mod generate_html;
-pub mod single_char_parser;
+mod wrapper;
 pub use wrapper::inline_code::process_inline_code;
 pub use wrapper::italics::process_asterisk as italics_asterisk;
 pub use wrapper::italics::process_underscore as italics_underscore;
